a811926b5732d50a7a6791d9dc59034ed65800aa
ngothuytien <40038901+ngothuytien@users.noreply.github.com>
2022-06-24 16:44:33 +0700

    code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutModule } from './layout/layout.module';
+import { FilterComponent } from './components/filter/filter.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'kohinoor-bangla': ['Kohinoor Bangla'],
+        'inter': ['Inter']
+      },
+      colors: {
+        'blue-logo': '#17ACAC',
+        'dark-blue': '#0E8585'
+      }
+    },
+  },
+  plugins: [],
+}
